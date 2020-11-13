183289e54153eeff6ffb97786af271c8850f08ac
Nsingletary <singletarybush@gmail.com>
2020-11-13 09:27:19 -0500

    commit tree 381bb8993918ea24672970a3e3cd04af74fd80b1

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,16 @@
 def unsafe?(speed)
+	if speed > 60 
+		true
+	elsif 
+		speed < 40
+		true
+	else
+		false
+	end
+end
 
-    if speed > 60
-      return true
-    elsif speed < 40
-      return true
-    else
-      return false
-    end
 
 
 def not_safe?(speed)
-    speed > 60 || speed < 40 ? true : false
-    end
-	
-
-
+	speed < 40 || speed > 60 ? true : false
+end
